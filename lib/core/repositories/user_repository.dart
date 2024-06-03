af45b943b0a1658966d15a8dcf948635ec9a6bd1
Hazem <hazemelnahas2010@gmail.com>
2024-06-03 15:56:39 +0300

    - add uploadImage Method in User Repository

--- lib/core/repositories/user_repository.dart
+++ lib/core/repositories/user_repository.dart
@@ -1,20 +1,24 @@
 import 'dart:convert';
+import 'dart:io';
 
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:shaghalni/core/data/database/cache/cache_helper.dart';
 import 'package:shaghalni/core/data/models/user_model.dart';
 import 'package:shaghalni/core/di/service_locator.dart';
 import 'package:shaghalni/core/utils/constants.dart';
 
 class UserRepository {
-
   final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+  final storageRef = FirebaseStorage.instance.ref();
 
-    Future<UserModel?> getUser() async {
+  Future<UserModel?> getUser() async {
     try {
-      DocumentSnapshot<Map<String, dynamic>> snapshot =
-          await _firestore.collection(FirestoreCollections.users).doc(FirebaseAuth.instance.currentUser!.uid).get();
+      DocumentSnapshot<Map<String, dynamic>> snapshot = await _firestore
+          .collection(FirestoreCollections.users)
+          .doc(FirebaseAuth.instance.currentUser!.uid)
+          .get();
       if (snapshot.exists) {
         return UserModel.fromJson(snapshot.data()!);
       } else {
@@ -25,11 +29,30 @@ class UserRepository {
     }
   }
 
-    Future<void> saveUserToPreferences(UserModel user) async {
-     await getIt<CacheHelper>().saveData(key: "user" , value:  jsonEncode(user.toJson()));
+  Future<String> uploadImage(File imageFile) async {
+    try {
+      // Generate a unique name for the image
+      final imageName = DateTime.now().millisecondsSinceEpoch.toString();
+      final imageRef = storageRef.child('images_users/$imageName');
+
+      // Upload the image
+      final uploadTask = imageRef.putFile(imageFile);
+
+      // Wait for the upload to complete
+      final downloadUrl = await uploadTask.snapshot.ref.getDownloadURL();
+
+      return downloadUrl;
+    } catch (e) {
+      rethrow;
+    }
   }
 
-   Future<UserModel?> getUserFromPreferences() async {
+  Future<void> saveUserToPreferences(UserModel user) async {
+    await getIt<CacheHelper>()
+        .saveData(key: "user", value: jsonEncode(user.toJson()));
+  }
+
+  Future<UserModel?> getUserFromPreferences() async {
     String? userJson = getIt<CacheHelper>().getData(key: 'user');
     if (userJson != null) {
       Map<String, dynamic> userMap = jsonDecode(userJson);
@@ -37,5 +60,4 @@ class UserRepository {
     }
     return null;
   }
-
 }
