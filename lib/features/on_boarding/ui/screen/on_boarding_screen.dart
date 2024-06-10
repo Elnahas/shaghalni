9f3f2dcc1b54039ead85db812c751b87fc7dc80c
Hazem <hazemelnahas2010@gmail.com>
2024-06-11 01:55:50 +0300

    - add app bar to can back or skip

--- lib/features/on_boarding/ui/screen/on_boarding_screen.dart
+++ lib/features/on_boarding/ui/screen/on_boarding_screen.dart
@@ -18,14 +18,39 @@ class _OnBoardingScreenState extends State<OnBoardingScreen> {
   PageController myController = PageController();
   bool isLastScreen = false;
 
+   @override
+  void dispose() {
+    myController.dispose();
+    super.dispose();
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        elevation: 0,
+        leading: IconButton(onPressed: (){
+          myController.previousPage(
+            duration: const Duration(milliseconds: 500),
+            curve: Curves.easeIn,
+          );
+        }, icon: Icon(Icons.arrow_back_ios,color: Colors.black,)),
+        actions: [
+          TextButton(
+              onPressed: () {
+                myController.animateToPage(3 , duration: const Duration(milliseconds: 500), curve: Curves.easeIn);
+              },
+              child: const Text(
+                "Skip",
+                style: TextStyle(color: Colors.black),
+              ))
+        ],
+      ),
       body: SafeArea(
         child: Padding(
           padding: const EdgeInsets.all(18),
           child: Column(
             children: [
+
               Expanded(
                 child: PageView.builder(
                   physics: const BouncingScrollPhysics(),
