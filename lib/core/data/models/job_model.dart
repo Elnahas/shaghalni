473d3f3ae7ab390eb4267143db0570946e234831
Hazem <hazemelnahas2010@gmail.com>
2024-06-19 13:00:13 +0300

    - add jobType to job model - set condition if not selected show msg - send jobType to save at firestore database

--- lib/core/data/models/job_model.dart
+++ lib/core/data/models/job_model.dart
@@ -1,108 +1,68 @@
 import 'package:json_annotation/json_annotation.dart';
 import 'package:shaghalni/core/data/enum/gender.dart';
+import 'package:shaghalni/core/data/enum/job_type.dart';
 import 'package:shaghalni/core/data/models/category_model.dart';
 import 'package:shaghalni/core/data/models/city_model.dart';
-
 import '../enum/job_status.dart';
+
 part 'job_model.g.dart';
+
+typedef Json = Map<String, dynamic>;
 
 @JsonSerializable()
 class JobModel {
   final String title;
   final String description;
-  @JsonKey(fromJson: _cityModelFromJson, toJson: _cityModelToJson)
+  @JsonKey(fromJson: CityModel.fromJson, toJson: _cityModelToJson)
   final CityModel city;
-  @JsonKey(fromJson: _categoryModelFromJson, toJson: _categoryModelToJson)
+  @JsonKey(fromJson: CategoryModel.fromJson, toJson: _categoryModelToJson)
   final CategoryModel category;
   final double salary;
   @JsonKey(name: 'is_hide_salary')
   final bool isHideSalary;
-  @JsonKey(fromJson: _statusFromJson, toJson: _statusToJson)
+  @JsonKey(fromJson: JobStatusExtension.fromString, toJson: _statusToJson)
   final JobStatus status;
-  @JsonKey(fromJson: _genderFromJson, toJson: _genderToJson)
+  @JsonKey(fromJson: GenderExtension.fromString, toJson: _genderToJson)
   final Gender gender;
+  @JsonKey(name: "job_type", fromJson: JobTypeExtension.fromString, toJson: _jobTypeToJson)
+  final JobType jobType;
   @JsonKey(name: 'created_at')
   final DateTime createdAt;
   @JsonKey(name: 'updated_at')
   final DateTime updatedAt;
   final int views;
-  @JsonKey(
-      name: 'posted_by', fromJson: _postedByFromJson, toJson: _postedByToJson)
+  @JsonKey(name: 'posted_by', fromJson: PostedBy.fromJson, toJson: _postedByToJson)
   final PostedBy? postedBy;
-  @JsonKey(
-      name: 'experience_range',
-      fromJson: _experienceRangeFromJson,
-      toJson: _experienceRangeToJson)
+  @JsonKey(name: 'experience_range', fromJson: ExperienceRange.fromJson, toJson: _experienceRangeToJson)
   final ExperienceRange? experienceRange;
 
-  factory JobModel.fromJson(Map<String, dynamic> json) =>
-      _$JobModelFromJson(json);
+  const JobModel({
+    required this.title,
+    required this.description,
+    required this.city,
+    required this.category,
+    required this.salary,
+    required this.isHideSalary,
+    required this.status,
+    required this.gender,
+    required this.jobType,
+    required this.createdAt,
+    required this.updatedAt,
+    required this.views,
+    this.postedBy,
+    required this.experienceRange,
+  });
 
-  JobModel(
-      {
-      required this.title,
-      required this.description,
-      required this.city,
-      required this.category,
-      required this.salary,
-      required this.isHideSalary,
-      required this.status,
-      required this.gender,
-      required this.createdAt,
-      required this.updatedAt,
-      required this.views,
-      this.postedBy,
-      required this.experienceRange});
+  factory JobModel.fromJson(Json json) => _$JobModelFromJson(json);
+  Json toJson() => _$JobModelToJson(this);
 
-  Map<String, dynamic> toJson() => _$JobModelToJson(this);
-
-  static CategoryModel _categoryModelFromJson(dynamic json) {
-    return CategoryModel.fromJson(json);
-  }
-
-  static dynamic _categoryModelToJson(CategoryModel category) {
-    return category.toJson();
-  }
-
-  static CityModel _cityModelFromJson(dynamic json) {
-    return CityModel.fromJson(json);
-  }
-
-  static dynamic _cityModelToJson(CityModel city) {
-    return city.toJson();
-  }
-
-  static JobStatus _statusFromJson(String status) {
-    return JobStatusExtension.fromString(status);
-  }
-
-  static String _statusToJson(JobStatus status) {
-    return status.toShortString();
-  }
-
-  static Gender _genderFromJson(String gender) {
-    return GenderExtension.fromString(gender);
-  }
-
-  static String _genderToJson(Gender gender) {
-    return gender.toShortString();
-  }
-
-  static PostedBy? _postedByFromJson(dynamic json) {
-    return json == null ? null : PostedBy.fromJson(json);
-  }
-
-  static dynamic _postedByToJson(PostedBy? postedBy) {
-    return postedBy?.toJson();
-  }
-
-  static ExperienceRange? _experienceRangeFromJson(dynamic json) {
-    return json == null ? null : ExperienceRange.fromJson(json);
-  }
-
-  static dynamic _experienceRangeToJson(ExperienceRange? experienceRange) {
-    return experienceRange?.toJson();
-  }
+  static Json _categoryModelToJson(CategoryModel category) => category.toJson();
+  static Json _cityModelToJson(CityModel city) => city.toJson();
+  static String _statusToJson(JobStatus status) => status.toShortString();
+  static String _genderToJson(Gender gender) => gender.toShortString();
+  static String _jobTypeToJson(JobType jobType) => jobType.toShortString();
+  static Json? _postedByToJson(PostedBy? postedBy) => postedBy?.toJson();
+  static Json? _experienceRangeToJson(ExperienceRange? experienceRange) => experienceRange?.toJson();
 }
 
 @JsonSerializable()
@@ -114,15 +74,14 @@ class PostedBy {
   @JsonKey(name: "phone_number")
   final String phoneNumber;
 
-  PostedBy({
-    required this.phoneNumber,
+  const PostedBy({
     required this.userId,
     required this.userName,
+    required this.phoneNumber,
   });
 
-  factory PostedBy.fromJson(Map<String, dynamic> json) =>
-      _$PostedByFromJson(json);
-  Map<String, dynamic> toJson() => _$PostedByToJson(this);
+  factory PostedBy.fromJson(Json json) => _$PostedByFromJson(json);
+  Json toJson() => _$PostedByToJson(this);
 }
 
 @JsonSerializable()
@@ -132,9 +91,11 @@ class ExperienceRange {
   @JsonKey(name: "max_experience")
   final int maxExperience;
 
-  ExperienceRange({required this.minExperience, required this.maxExperience});
+  const ExperienceRange({
+    required this.minExperience,
+    required this.maxExperience,
+  });
 
-  factory ExperienceRange.fromJson(Map<String, dynamic> json) =>
-      _$ExperienceRangeFromJson(json);
-  Map<String, dynamic> toJson() => _$ExperienceRangeToJson(this);
+  factory ExperienceRange.fromJson(Json json) => _$ExperienceRangeFromJson(json);
+  Json toJson() => _$ExperienceRangeToJson(this);
 }
