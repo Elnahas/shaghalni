05fdbc0cc8e55f94ed52fbcecc7c338779986e36
Hazem <hazemelnahas2010@gmail.com>
2024-06-21 14:33:45 +0300

    - create Route for category and call getCategories after create screen

--- lib/core/functions/show_modal_bottom_sheet.dart
+++ lib/core/functions/show_modal_bottom_sheet.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:shaghalni/core/helpers/extentions.dart';
-import 'package:shaghalni/core/routing/routes.dart';
 
  onWillPop(BuildContext context, String title)  {
 
