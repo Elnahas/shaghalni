058ddd38cc39d490cf06acb5ec33271ce87c9735
Hazem <hazemelnahas2010@gmail.com>
2024-06-21 14:43:44 +0300

    - Navigator to CategoryScreen after click view All

--- lib/features/home/ui/widgets/home_sections/category_list_section.dart
+++ lib/features/home/ui/widgets/home_sections/category_list_section.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:shaghalni/core/data/models/category_model.dart';
+import 'package:shaghalni/core/helpers/extentions.dart';
+import 'package:shaghalni/core/routing/routes.dart';
 import 'package:shaghalni/core/widgets/custom_header_section.dart';
 import 'package:shaghalni/features/home/ui/widgets/category_items_widget.dart';
 import '../../../../../core/helpers/spacing.dart';
@@ -13,8 +15,12 @@ class CategoryListSection extends StatelessWidget {
   Widget build(BuildContext context) {
     return Column(
       children: [
-        const CustomHeaderSection(
+        CustomHeaderSection(
           text: "Categories",
+          onPressed: () {
+            
+            context.pushNamed(Routes.category);
+          },
         ),
         verticalSpace(10),
         SizedBox(
@@ -23,7 +29,11 @@ class CategoryListSection extends StatelessWidget {
           child: ListView.builder(
             physics: const BouncingScrollPhysics(),
             scrollDirection: Axis.horizontal,
-            itemBuilder: (context, index) => CategoryItemsWidget(index: index , categoryModel:categoryList[index] , length: categoryList.length,),
+            itemBuilder: (context, index) => CategoryItemsWidget(
+              index: index,
+              categoryModel: categoryList[index],
+              length: categoryList.length,
+            ),
             itemCount: categoryList.length,
           ),
         )
