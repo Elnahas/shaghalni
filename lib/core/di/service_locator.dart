81251e2ec5e1f67497c38f1f23c607c5628f6adf
Hazem <hazemelnahas2010@gmail.com>
2024-06-21 14:31:37 +0300

    - register CategoryCubit in setupServiceLocator

--- lib/core/di/service_locator.dart
+++ lib/core/di/service_locator.dart
@@ -6,13 +6,14 @@ import 'package:shaghalni/features/add_job/logic/cubit/add_job_cubit.dart';
 import 'package:shaghalni/features/auth/login/logic/cubit/login_cubit.dart';
 import 'package:shaghalni/features/auth/otp/logic/cubit/otp_cubit.dart';
 import 'package:shaghalni/features/auth/signup/logic/cubit/signup_cubit.dart';
+import 'package:shaghalni/features/category/logic/category_cubit.dart';
 import '../../features/job_details/logic/job_details_cubit.dart';
 import '../../features/language/logic/language_cubit.dart';
 import '../repositories/auth_repository.dart';
 import '../repositories/city_repository.dart';
 
 final getIt = GetIt.instance;
-void setupServiceLocator( ) {
+void setupServiceLocator() {
   getIt.registerSingleton<AuthRepository>(AuthRepository());
   getIt.registerSingleton<CategoryRepository>(CategoryRepository());
   getIt.registerSingleton<CityRepository>(CityRepository());
@@ -32,6 +33,8 @@ void setupServiceLocator( ) {
 
   getIt.registerSingleton<LanguageCubit>(LanguageCubit());
 
-  getIt.registerFactory<JobDetailsCubit>(() => JobDetailsCubit(getIt<JobRepository>()));
-
+  getIt.registerFactory<JobDetailsCubit>(
+      () => JobDetailsCubit(getIt<JobRepository>()));
+  getIt.registerFactory<CategoryCubit>(
+      () => CategoryCubit(getIt<CategoryRepository>()));
 }
