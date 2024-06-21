017c89e0a8cb8250d7967fc6936b010b75c3bc1b
Hazem <hazemelnahas2010@gmail.com>
2024-06-21 14:29:39 +0300

    - add getCategories in CategoryCubit

--- lib/features/category/logic/category_cubit.dart
+++ lib/features/category/logic/category_cubit.dart
@@ -1,7 +1,24 @@
 import 'package:bloc/bloc.dart';
+import 'package:shaghalni/core/repositories/category_repository.dart';
 import 'package:shaghalni/features/category/logic/category_state.dart';
 
 
 class CategoryCubit extends Cubit<CategoryState> {
-  CategoryCubit() : super(CategoryState.initial());
+
+  final CategoryRepository _categoryRepository;
+
+  CategoryCubit(this._categoryRepository) : super(CategoryState.initial());
+
+
+  Future<void> getCategories() async {
+    try {
+      emit(CategoryState.categoryLoading());
+      var categoryList = await _categoryRepository.getCategories();
+      emit(CategoryState.categorySuccess(categoryList));
+    } catch (e) {
+      emit(CategoryState.categoryFailure(e.toString()));
+    }
+  }
+
+  
 }
