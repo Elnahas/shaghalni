f24186197a1c8d1235a1b8386677c67c105df77d
Hazem <hazemelnahas2010@gmail.com>
2024-07-02 22:57:51 +0300

    - create function in blog cubit to get blogs and make class cubit inject and register at service locator

--- lib/features/blog/logic/cubit/blog_cubit.dart
+++ lib/features/blog/logic/cubit/blog_cubit.dart
@@ -1,7 +1,19 @@
 import 'package:bloc/bloc.dart';
+import 'package:shaghalni/features/blog/data/repo/blog_repository.dart';
 import 'package:shaghalni/features/blog/logic/cubit/blog_state.dart';
 
 
 class BlogCubit extends Cubit<BlogState> {
-  BlogCubit() : super(BlogState.initial());
+  final BlogsRepository _blogsRepository ;
+  BlogCubit(this._blogsRepository) : super(BlogState.initial());
+
+  Future<void> getBlogs() async {
+    emit(BlogState.blogLoading());
+    try {
+      final blogs = await _blogsRepository.getBlogs();
+      emit(BlogState.blogSuccess(blogs));
+    } catch (e) {
+      emit(BlogState.blogFailure(e.toString()));
+    }
+  }
 }
