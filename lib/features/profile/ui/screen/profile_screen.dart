3eb9e360a97c493d83e5ef120c8d7bc4f91cbf04
Hazem <hazemelnahas2010@gmail.com>
2024-07-25 12:14:22 +0300

    - section setting by list ui

--- lib/features/profile/ui/screen/profile_screen.dart
+++ lib/features/profile/ui/screen/profile_screen.dart
@@ -14,10 +14,51 @@ class ProfileScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        margin: EdgeInsets.all(20),
-        child: Column(
-          children: [UserProfileRow()],
+      body: SingleChildScrollView(
+        physics: const BouncingScrollPhysics(),
+        child: Container(
+          margin: EdgeInsets.all(20),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const UserProfileRow(),
+              verticalSpace(30),
+              Text(
+                "Settings",
+                style: AppTextStyles.font18BoldBlack,
+              ),
+              verticalSpace(10),
+              Container(
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(12),
+                  boxShadow: [
+                    BoxShadow(
+                      color: AppColors.primaryColor.withOpacity(0.1),
+                      spreadRadius: 1,
+                      blurRadius: 8,
+                      offset: const Offset(0, 4),
+                    ),
+                  ],
+                ),
+                child: ListView.separated(
+                    physics: const NeverScrollableScrollPhysics(),
+                    shrinkWrap: true,
+                    itemBuilder: (context, index) => ListTile(
+                      trailing: Icon(Icons.chevron_right),
+                        leading: Icon(FontAwesomeIcons.user),
+                        title: Text("Profile")),
+                        
+                    separatorBuilder: (context, index) => Padding(
+                          padding: const EdgeInsets.symmetric(horizontal: 20),
+                          child: Divider(
+                            color: Colors.grey[200],
+                          ),
+                        ),
+                    itemCount: 10),
+              )
+            ],
+          ),
         ),
       ),
     );
